e33514292224ab84fab26eac1b95b4ff00ab1eff
Mike McClurg <mike.mcclurg@gmail.com>
2015-05-17 23:02:31 -0600

    Examples for parts 2 and 3

---
+++ example_script_part_2.sh
@@ -0,0 +1,17 @@
+# Example Script Part 2 - Software-as-a-Service
+
+# Go to your Desktop
+cd ~/Desktop
+
+# Make a folder for Girl Develop It, and go
+mkdir GDI
+cd GDI
+
+# Let's download a sample web project:
+git clone https://github.com/mcclurmc/html5demos.git
+
+# Go into that directory
+cd html5demos
+
+# Let's view a sample web page
+open demos/drag.html
---
+++ example_script_part_3.sh
@@ -0,0 +1,30 @@
+# Example Script Part 3 - Platform-as-a-Service
+
+# Make sure that you've installed the Heroku Toolbelt:
+open https://toolbelt.heroku.com/
+
+# If you haven't already done so, make an account:
+open https://toolbelt.heroku.com/
+
+# Log in to Heroku Toolbelt (don't forget your credentials from the
+# previous step):
+heroku login
+
+# Let's download a sample Ruby on Rails app:
+cd ~/Desktop/GDI
+git clone https://github.com/mcclurmc/ruby-rails-sample.git
+cd ruby-rails-sample
+
+# Now let's run the app on Heroku!
+heroku create
+git push heroku master
+heroku run rake db:migrate
+heroku open
+
+# Let's make a change! Open up the welcome page, edit it to say
+# "Welcome <Your Name>", commit, and redeploy.
+nano app/views/welcome/index.html.erb
+git add .
+git commit -m "My first commit"
+git push heroku master
+heroku open
