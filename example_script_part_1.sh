871b6d88e186baff5e707f4c47b052cab16e9129
Mike McClurg <mike.mcclurg@gmail.com>
2015-05-17 22:47:23 -0600

    Add part 1 examples

---
+++ example_script_part_1.sh
@@ -0,0 +1,32 @@
+# Example Script Part 1 - Intro to the command line (OS X or Linux (mostly))
+
+# This is a comment. If you type this into the shell, it won't be executed.
+
+# Go to your home directory by typing 'cd' by itself.
+cd
+
+# What is the file path to your home directory?
+pwd
+
+# What files are in your home directory?
+ls
+
+# Go to your Desktop. The ~ (tilde) character represents your home directory, 
+cd ~/Desktop
+
+# What files are on your Desktop?
+ls
+
+# Let's download a file from the Internet (don't worry, this is safe)
+wget http://photos3.meetupstatic.com/photos/event/6/7/b/8/highres_397346552.jpeg -O GDI.jpg
+
+# Pick a file, and type 'open the_file_name'. What happens?
+open GDI.jpg
+
+# What do you think this command does?
+say "Hello Girl Develop It!"
+
+# How does that command work? ('man' is short for 'manual')
+man say
+
+# Can you use the manual to make your computer speak in different voices?
